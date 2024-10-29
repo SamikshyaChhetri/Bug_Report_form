d78727d1bf474defa0ac499a8904db2f9b97c917
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-10-29 15:13:47 +0545

    select shadcn ui

--- src/Component.tsx
+++ src/Component.tsx
@@ -1,7 +1,15 @@
 import { toast, Toaster } from "sonner";
 import { Button } from "./components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "./components/ui/dialog";
-
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 const Component = () => {
   return (
     <div className="flex gap-4">
@@ -13,6 +21,21 @@ const Component = () => {
       >
         shadcn Button
       </Button>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 
       <Dialog>
         <DialogTrigger>Open</DialogTrigger>
