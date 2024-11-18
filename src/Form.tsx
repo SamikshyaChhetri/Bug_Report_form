f58d9ab0168ed9a81b805b850d919e150d348606
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-11-18 15:02:55 +0545

    displaying the value in console

--- src/Form.tsx
+++ src/Form.tsx
@@ -19,8 +19,18 @@ import {
   SelectValue,
 } from "./components/ui/select";
 import { Textarea } from "./components/ui/textarea";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const newForm = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      contact: "",
+      message: "",
+      application: "",
+    },
+  });
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Card className=" w-[50%]">
@@ -34,16 +44,30 @@ const Form = () => {
           <div className="flex flex-col gap-5">
             <div className="grid w-full  items-center gap-1.5">
               <label htmlFor="text">Username</label>
-              <Input type="text" id="text" placeholder="Username" />
+              <Input
+                type="text"
+                id="text"
+                placeholder="Username"
+                {...newForm.register("username")}
+              />
             </div>
 
             <div className="grid w-full items-center gap-1.5">
               <label htmlFor="email">Email</label>
-              <Input type="email" id="email" placeholder="Email" />
+              <Input
+                type="email"
+                id="email"
+                placeholder="Email"
+                {...newForm.register("email")}
+              />
             </div>
             <div className="grid w-full  items-center gap-1.5">
               <label htmlFor="number">Contact</label>
-              <Input type="number" id="number" placeholder="Contact" />
+              <Input
+                type="number"
+                placeholder="Contact"
+                {...newForm.register("contact")}
+              />
             </div>
             <div>
               <label htmlFor="apps">Select your favourite app</label>
@@ -66,7 +90,10 @@ const Form = () => {
             </div>
             <div className="grid w-full  items-center gap-1.5">
               <label htmlFor="msg">Message</label>
-              <Textarea placeholder="Type your message here." />
+              <Textarea
+                placeholder="Type your message here."
+                {...newForm.register("message")}
+              />
             </div>
           </div>
         </CardContent>
@@ -83,7 +110,9 @@ const Form = () => {
           <Button
             className="bg-green-700 hover:bg-green-600"
             onClick={() => {
-              toast.success("Form submitted successfully"); // Optional duration in ms
+              toast.success("Form submitted successfully");
+              const values = newForm.getValues();
+              console.log(values);
             }}
           >
             Deploy
